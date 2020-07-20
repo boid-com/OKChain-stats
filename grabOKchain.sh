080cd4d5140464a2720c8607461c80d24f71a954
ghobson2013 <ugac007@gmail.com>
2020-07-20 15:30:28 +0200

    added tokt supply and bonded_token count

--- grabOKchain.sh
+++ grabOKchain.sh
@@ -10,6 +10,8 @@ START="$(date +%s)"
 OKCHAINVERSION=`okchaincli status | jq '.node_info.version'| tr -d '"'`
 OKCHAIN_LASTBLK=`okchaincli status | jq '.sync_info.latest_block_height' | tr -d '"'`
 OKCHAIN_MONIKER=`okchaincli status | jq '.node_info.moniker'| tr -d '"'`
+OKCHAIN_TOKENS=`okchaincli  query staking pool | jq '.bonded_tokens' | tr -d '"'`
+OKCHAIN_TOKT=`okchaincli  query token info tokt | jq '.total_supply' | tr -d '"'`
 
 echo "$OKCHAIN_MONIKER v$OKCHAINVERSION $OKCHAIN_LASTBLK"
 
@@ -20,6 +22,8 @@ okchain_duration_seconds $(($END - $START))
 okchain_last_run_seconds $END
 okchain_last_block{name="$OKCHAIN_MONIKER"} $OKCHAIN_LASTBLK
 okchain_version{version="$OKCHAINVERSION"} 1
+okchain_bonded_tokens $OKCHAIN_TOKENS
+okchain_tokt_supply $OKCHAIN_TOKT
 EOF
 
 # Rename the temporary file atomically.
